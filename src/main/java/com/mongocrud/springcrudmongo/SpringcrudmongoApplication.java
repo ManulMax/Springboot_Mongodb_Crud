6f7be5ba055e33f02181de739a8d8e3609b3e280
ManulMax <manulmadara9725@gmail.com>
2021-10-13 18:02:24 +0530

    add Docket and ApiInfo

--- src/main/java/com/mongocrud/springcrudmongo/SpringcrudmongoApplication.java
+++ src/main/java/com/mongocrud/springcrudmongo/SpringcrudmongoApplication.java
@@ -1,13 +1,17 @@
 package com.mongocrud.springcrudmongo;
 
+import io.swagger.models.Contact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -21,8 +25,19 @@ public class SpringcrudmongoApplication {
     public Docket swaggerConfiguration(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .paths(PathSelectors.ant("/api/*"))
+//                .paths(PathSelectors.ant("/*"))
                 .apis(RequestHandlerSelectors.basePackage("com.mongocrud"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My Test API",
+                "This API Only For Test Purposes",
+                "1.0",
+                "Terms of service",
+                new springfox.documentation.service.Contact("Manul", "manul.com", "manul@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
     }
 }
